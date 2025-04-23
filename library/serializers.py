4b17c2c0b2e7f5d2e6b6fcd829f6cfc8934e9176
Pahankov <pahankov@mail.ru>
2025-04-23 20:36:25 +0300

    run

--- library/serializers.py
+++ library/serializers.py
@@ -2,31 +2,53 @@ from rest_framework import serializers
 from .models import Book, Order
 
 
-class BookSerializer(serializers.ModelSerializer):
+# Сериализатор для списка книг (минимум информации)
+class BookListSerializer(serializers.ModelSerializer):
     orders_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'author', 'year', 'orders_count']
 
     def get_orders_count(self, obj):
         return obj.order_set.count()
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    # Для записи используем PrimaryKeyRelatedField
-    books = serializers.PrimaryKeyRelatedField(
+# Сериализатор для деталей книги (полная информация)
+class BookDetailSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def get_orders(self, obj):
+        return obj.order_set.values_list('id', flat=True)
+
+
+# Сериализатор для списка заказов
+class OrderListSerializer(serializers.ModelSerializer):
+    book_titles = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user_name', 'days_count', 'date', 'book_titles']
+
+    def get_book_titles(self, obj):
+        return [book.title for book in obj.books.all()]
+
+
+# Сериализатор для деталей заказа
+class OrderDetailSerializer(serializers.ModelSerializer):
+    books = BookListSerializer(many=True, read_only=True)
+
+    # Для ввода используем PrimaryKeyRelatedField
+    book_ids = serializers.PrimaryKeyRelatedField(
         many=True,
         queryset=Book.objects.all(),
-        write_only=True,  # Только для ввода, не для вывода
-        required=True
-    )
-
-    # Для чтения используем BookSerializer
-    book_details = BookSerializer(
         source='books',
-        many=True,
-        read_only=True
+        write_only=True,
+        required=True
     )
 
     class Meta:
@@ -36,24 +58,6 @@ class OrderSerializer(serializers.ModelSerializer):
             'user_name',
             'days_count',
             'date',
-            'books',  # Для ввода (ID книг)
-            'book_details'  # Для вывода (полные данные книг)
+            'books',  # полная информация (только для чтения)
+            'book_ids'  # только ID (только для записи)
         ]
-        extra_kwargs = {
-            'books': {'required': True}
-        }
-
-    def to_representation(self, instance):
-        """Переопределяем вывод, чтобы сохранить вашу логику"""
-        representation = super().to_representation(instance)
-
-        # Ваша существующая логика
-        if 'list' in self.context.get('view_action', ''):
-            representation['books'] = BookSerializer(
-                instance.books.all(),
-                many=True
-            ).data
-
-        # Удаляем дублирование данных
-        representation.pop('book_details', None)
-        return representation
